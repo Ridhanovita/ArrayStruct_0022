8cb504b3f12fb00f7d776faec90c5e8c2524869a
Ridhanovita <novitaridha796@gmail.com>
2025-04-14 10:51:25 +0700

    memberi nilai pada elemen array per index

--- array.cpp
+++ array.cpp
@@ -9,3 +9,10 @@ string nama[5]= {"ben", "agus", "fara", "ama", "aqila"};
 
 //mendeklarasikan array dengan ukuran dan tanpa nilai awal
 float score[5];
+
+int main(){
+    cout << " Nama pada index 4 adalah " << nama[3] << endl;
+    //memberi nilai pada elemen array per index
+    nama[3]= "Ridha";
+    cout << "Nama pada index 4 adalah " << nama[3] << endl;
+}
