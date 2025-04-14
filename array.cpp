f1ea9d04e92b5b062f0d0f24d2e37b7c9341df9f
Ridhanovita <novitaridha796@gmail.com>
2025-04-14 10:46:57 +0700

    mendeklarasikan array dengan ukuran dan diberi nilai awal

--- array.cpp
+++ array.cpp
@@ -3,3 +3,7 @@ using namespace std;
 
 //mendeklarasikan array tanpa ukuran, wajib diberi nilai awal
 int nilai[]={1,2,3};
+
+//mendeklarasikan array dengan ukuran dan diberi nilai awal
+string nama[5]= {"ben", "agus", "fara", "ama", "aqila"};
+
